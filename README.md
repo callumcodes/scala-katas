867ff211170fb5f44e358327eee32dd8ec2e513c
Callum Turnbull <callum@turnbull.codes>
2016-11-06 16:36:04 +0000

    Update command line args

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Based on https://www.codewars.com/kata/alan-partridge-i-partridge-watch
 
 ## To Run
-`sbt run {sentence}`
+`sbt "run {sentence}"`
 
 ## Run Test Suite
 `sbt test`
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,5 @@
 object Main extends App {
   override def main(args: Array[String]): Unit = {
-    if(args.length != 1) println("Sentence should be one argument")
-    else println(Partridge.chat(args(0).split(' ').toList))
+    println(Partridge.chat(args.toList))
   }
 }
